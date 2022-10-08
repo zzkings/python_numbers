e7f8871d884b353ab33c9b6f04d052a3643ca735
jcgir <jcgiraldo654@gmail.com>
2022-10-08 18:50:42 -0400

    Random

---
+++ Random.py
@@ -0,0 +1,31 @@
+# random number guessing game
+
+import random
+
+flag = True
+while flag:
+    num = input('Type a number for a upper bound: ')
+
+    if num.isdigit() :
+      print("Lets Play!")
+      num= int(num)
+      flag = False
+    else:
+        print('Invalid input! Try again!')
+
+secret = random.randint(1,num)
+
+guess = None
+count = 1
+
+while guess != secret:
+    guess = input('Please type a number between 1 and ' + str(num) + ": ")
+    if guess.isdigit():
+        guess = int(guess)
+
+    if guess == secret:
+        print('You got it')
+    else:
+        print('Please try again')
+        count += 1
+print('It took you', count, 'guesses')
